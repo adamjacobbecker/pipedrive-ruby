94f0c867bd9fd5f4356e19223c4ac33fd38ab94b
Adam Becker <ad@mbecker.cc>
2014-02-13 09:07:57 -0500

    Update stage.rb

--- lib/pipedrive/stage.rb
+++ lib/pipedrive/stage.rb
@@ -1,7 +1,7 @@
 module Pipedrive
   class Stage < Base
-    def self.deals(id)
-      Deal.all(get "#{resource_path}/#{id}/deals", :everyone => 1)
+    def self.deals(id, opts = {})
+      Deal.all(get "#{resource_path}/#{id}/deals", query: { everyone: 1 }.merge(opts))
     end
   end
 end
